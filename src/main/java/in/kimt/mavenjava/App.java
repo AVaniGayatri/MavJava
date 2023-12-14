4028d8a4483932f895773149a7e2dd73b44ad902
AVaniGayatri <avg0828gmail.com>
2023-12-14 20:16:50 +0530

    Demo fot test

---
+++ src/main/java/in/kimt/mavenjava/App.java
@@ -0,0 +1,13 @@
+package in.kimt.mavenjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
